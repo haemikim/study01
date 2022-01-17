6243b68636076d7911d445efae54fdf43cb7dea5
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-01-17 18:46:51 +0900

    2022.01.17 페이징부분 설명 추가

--- SpringEx/src/main/webapp/resources/js/list.js
+++ SpringEx/src/main/webapp/resources/js/list.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
 	$(".paginate_button a").on("click",function(e){
 		//alert("aaa")
 		e.preventDefault();
-		// form태그안에 있는 pageNum 값을 가지고 와라
+		// form태그안에 있는 pageNum 값을 가지고 와라           	// this : 내가 선택한 페이지 넘버
 		actionForm.find("input[name='pageNum']").val($(this).attr("href"))
 		// actionForm.find("input[name='pageNum']") = <input type="text" name="pageNum" value="${pageMaker.cri.pageNum}"> 
 		// val  = ${pageMaker.cri.pageNum}
-		// attr("href") = 내가 선택한 값을 ${num}에 넣어라
+		// attr("href") = 내가 선택한 값(페이지 넘버)을 "${pageMaker.startPage-1}, ${num}, ${pageMaker.endPage+1}에 넣어라
 		//form태그 안에 있는 값을  controller로 보내라
 		actionForm.submit();
 	})//$("a")로 작성하면 a태그 전부가 선택된다
@@ -22,7 +22,7 @@ $(document).ready(function(){
 	$("input[type='submit']").on("click",function(e){
 		e.preventDefault();
 		// pageNum의 값을 1로 설정(검색결과에 대한 페이지와 내가 있는 페이지가 달라도 검색히면 첫번째 페이지로 가게 해준다)
-		actionForm.find("input[name='pageNum']").val("1")
+		actionForm.find("input[name='pageNum']").val("1") // 결과값이 1번째 페이지부터 나오돌고 설정
 		actionForm.submit();
 	})
 })
